d36a9d0ee3107ca2e55b8aeffc46a996ef40edfa
Sreehari k p <114796205+sreehari-k-p@users.noreply.github.com>
2023-05-08 15:25:00 +0530

    Add files via upload

---
+++ arrayobj.js
@@ -0,0 +1,14 @@
+let arr= [
+    {
+        name:'Sreehari',
+        age:19,
+        place:'Pbvr',    
+        },
+        {
+            name:'Soorya',
+            age:19,
+            place:'Okkal',    
+            }
+]
+// console.log(arr);
+console.log(`${arr[0].name} lives in ${arr[0].place}`)
